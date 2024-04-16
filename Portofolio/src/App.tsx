639fd3906da701a17458a7a2c182bfe5e3b21889
Rahmah Putri Azzahra <rahmaputriazzahra@gmail.com>
2024-04-16 10:08:36 +0700

    chore: setup

---
+++ Portofolio/src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <>
+    <div className='container'>
+      <h1>Rahmah Putri Azzahra</h1>
+      <h2>Hacktiv8 FSJS Graduate</h2>
+
+      <div>
+        <h3>Projects</h3>
+        <ul>
+          <li>Project 1</li>
+          <li>Project 2</li>
+          <li>Project 3</li>
+        </ul>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
