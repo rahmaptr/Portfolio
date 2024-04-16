e8ee499f9b3eef69bba50c1243275ed3961253cd
Rahmah Putri Azzahra <rahmaputriazzahra@gmail.com>
2024-04-16 10:58:36 +0700

    feat: add footer

--- Portofolio/src/App.tsx
+++ Portofolio/src/App.tsx
@@ -1,3 +1,5 @@
+import Footer from "./components/Footer"
+
 function App() {
   return (
     <>
@@ -14,6 +16,7 @@ function App() {
         </ul>
       </div>
     </div>
+    <Footer />
     </>
   )
 }
---
+++ Portofolio/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      Made with React, TypeScript, and ❤️ by <a href="linkedin.com/in/rahmah-putri-azzahra">Rahma</a>
+    </footer>
+  )
+}
+
+export default Footer
