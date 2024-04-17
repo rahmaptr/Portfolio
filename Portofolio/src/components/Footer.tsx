3634babaec616cca135f77136584bde5b876acb3
Rahmah Putri Azzahra <rahmaputriazzahra@gmail.com>
2024-04-17 22:29:39 +0700

    style: heading

--- Portofolio/src/components/Footer.tsx
+++ Portofolio/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="w-screen text-center">
-      <h3 className="w-full">Made with React, TypeScript, and ❤️ by <a href="linkedin.com/in/rahmah-putri-azzahra">Rahma</a></h3>
+      <h6 className="w-full">Made with React, TypeScript, and ❤️ by <a href="linkedin.com/in/rahmah-putri-azzahra">Rahma</a></h6>
     </footer>
   )
 }
--- Portofolio/src/routes/home.tsx
+++ Portofolio/src/routes/home.tsx
@@ -2,10 +2,10 @@ function Home() {
   return (
     <div className="px-5">
       <h1>Rahmah Putri Azzahra</h1>
-      <h2>Hacktiv8 FSJS Graduate</h2>
+      <h4>Hacktiv8 FSJS Graduate</h4>
 
       <div>
-        <h3>Projects</h3>
+        <h3 className="text-center">Projects</h3>
         <ul>
           <li>Project 1</li>
           <li>Project 2</li>
